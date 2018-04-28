51f568259a51b053f81a5a354cf35eaa936aed6d
lixin3 <1280064573@qq.com>
2018-04-28 09:51:22 +0800

    lixin

--- router/productRouter.js
+++ router/productRouter.js
@@ -20,4 +20,11 @@ router.post('/addProductType.do',controller.addProductType);//增加商品类型
 router.post('/updateProductType.do',controller.updateProductType);//修改商品类型
 router.post('/deleteProductType.do',controller.deleteProductType);//删除商品类型
 
+router.post('/producttypenum.do',controller.producttypenum);//获取商品类型数据量（新）
+
+router.post('/productContent.do',controller.productContent)//单件商品详情内容
+router.post('/productComments.do',controller.productComments)//单件商品评论内容
+router.post('/deleteComments.do',controller.deleteComments)//删除一条评论内容
+
+
 module.exports=router;
