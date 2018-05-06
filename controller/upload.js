e8561b3067756349936cd06ada8628dc1a8935a2
lixin3 <1280064573@qq.com>
2018-05-06 18:14:18 +0800

    .

--- controller/upload.js
+++ controller/upload.js
@@ -10,18 +10,15 @@ module.exports={
         var upload=muilter.array('txtFile');
         upload(req, res, function (err) {
             //添加错误处理
-            console.log(err);
-
             if (err) {
                 return  console.log(err);
             }
             //文件信息在req.file或者req.files中显示。
-            console.log(req.files[0]);
             let uploadsrc =req.files[0].destination+"/";
             let uploadfilename = req.files[0].filename;
             let uploadsrcstring= uploadsrc+uploadfilename;
-            uploadsrcstring = uploadsrcstring.substring(8);
-            uploadsrcstring = "http://localhost:9999"+uploadsrcstring;
+            uploadsrcstring = uploadsrcstring.substring(2);
+            uploadsrcstring = "http://localhost:9999/"+uploadsrcstring;
             console.log(uploadsrcstring);
             res.send(uploadsrcstring)
         });
